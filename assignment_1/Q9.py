bd0f908cbe63ee6d6a6125ec06cc79951935aae7
Shubhlata3966 <134135473+Shubhlata3966@users.noreply.github.com>
2023-05-21 00:52:57 +0530

    Add files via upload

---
+++ assignment_1/Q9.py
@@ -0,0 +1,5 @@
+name="Abhisehk Kumar Kesharwani"
+age=21
+qualification="I'm currently B.tech 4th year"
+experience="Currently i have no experience because i'm fresher"
+print("My name is:",name,"\n" "Age:",age, "\n" "Qualification:",qualification, "\n" "Experience:",experience )
